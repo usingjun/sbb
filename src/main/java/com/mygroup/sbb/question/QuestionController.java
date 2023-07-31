e184e7933ede44a4291bb79e7ede85b07fe174b8
yong <usingjun99@gmail.com>
2023-07-31 11:55:48 +0900

    Apply pagenation

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -2,6 +2,7 @@ package com.mygroup.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,9 +25,12 @@ public class QuestionController {
     private final QuestionService questionService;
     
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(
+        Model model, 
+        @RequestParam(value = "page", defaultValue = "0") int page
+        ) {
+        Page<Question> paging=this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
