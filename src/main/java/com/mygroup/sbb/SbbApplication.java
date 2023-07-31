6bdedff6e164de70ceaec59774b30e0591dde191
yong <usingjun99@gmail.com>
2023-07-31 10:31:09 +0900

    Initial commit

---
+++ src/main/java/com/mygroup/sbb/SbbApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mygroup/sbb/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mygroup.sbb.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
